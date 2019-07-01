20be2d81912d10c9cf1510c49c13c6849f6c8c71
FarahJemai599 <52163889+FarahJemai599@users.noreply.github.com>
2019-07-01 16:35:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var ta=document.getElementById("text");
+function bold(){
+    ta.style.fontWeight="bold";
+}
+function italic(){
+    ta.style.fontStyle="italic";
+}
+function underline(){
+    ta.style.textDecoration="underline";
+}
+function px(){
+    ta.style.fontSize=document.getElementById("px").value;
+}
+function police(){
+    ta.style.fontFamily=document.getElementById("police").value;
+}
